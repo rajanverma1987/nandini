5424f91d5df66966259a26f267febffce863d0b6
rajan vermam <rajanverma1987@gmail.com>
2023-07-11 10:19:10 -0600

    Menu collapsible

--- src/components/Layout/sidemenu/Sidemenu.js
+++ src/components/Layout/sidemenu/Sidemenu.js
@@ -3,12 +3,60 @@ import routes from "../../../constant/routes";
 import styles from "./styles/sidemenue.module.css";
 import { useContext } from "react";
 import { Context } from "../../../store/store";
+import { BsFillCaretDownFill } from "react-icons/bs";
+import { BsFillCaretUpFill } from "react-icons/bs";
 
 export default function SideMenu() {
   const { activeTab, activateTab } = useContext(Context);
 
   function changeTab(child) {
     activateTab(child, true);
+  }
+
+  function MenuList({ route, routeIndex }) {
+    const [expand, setExpand] = useState(false);
+    return (
+      <ul key={`route_${routeIndex}`}>
+        <p
+          className={styles.ulHeader}
+          onClick={(e) => {
+            e.preventDefault();
+            setExpand((prev) => !prev);
+          }}
+        >
+          <span>
+            <route.icon /> {route.name}
+          </span>
+          <span>
+            {expand ? (
+              <BsFillCaretUpFill
+                onClick={() => {
+                  setExpand((prev) => !prev);
+                }}
+              />
+            ) : (
+              <BsFillCaretDownFill
+                onClick={() => {
+                  setExpand((prev) => !prev);
+                }}
+              />
+            )}
+          </span>
+        </p>
+        {expand &&
+          route?.children.map((child, index) => {
+            return (
+              <li
+                onClick={changeTab.bind(this, child)}
+                className={activeTab.name === child.name ? styles.active : ""}
+                key={`menu${index}`}
+              >
+                {child.name}
+              </li>
+            );
+          })}
+      </ul>
+    );
   }
   return (
     <>
@@ -20,24 +68,7 @@ export default function SideMenu() {
           routes.length > 0 &&
           routes.map((route, routeIndex) => {
             return route?.children ? (
-              <ul key={`route_${routeIndex}`}>
-                <p>
-                  <route.icon /> {route.name}
-                </p>
-                {route?.children.map((child, index) => {
-                  return (
-                    <li
-                      onClick={changeTab.bind(this, child)}
-                      className={
-                        activeTab.name === child.name ? styles.active : ""
-                      }
-                      key={`menu${index}`}
-                    >
-                      {child.name}
-                    </li>
-                  );
-                })}
-              </ul>
+              <MenuList route={route} routeIndex={routeIndex} />
             ) : (
               <ul key={`rounte${routeIndex}`}>
                 <p
